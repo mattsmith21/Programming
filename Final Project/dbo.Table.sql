a547128b09aa1c91025a392fda21c86aaa69556b
mattsmith21 <60450489+mattsmith21@users.noreply.github.com>
2023-12-01 23:01:40 -0500

    Add files via upload

---
+++ Final Project/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ID] VARCHAR(50) NULL, 
+    [Subject] VARCHAR(50) NULL, 
+    [Requester] VARCHAR(50) NULL, 
+    [Assigned To] VARCHAR(50) NULL, 
+    [Creation Date] VARCHAR(50) NULL, 
+    [Priority] VARCHAR(50) NULL
+)
